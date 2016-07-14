29008b8064f9a4beec61af2ca7e5e27bc3736620
Ian Walton <itwalton90@gmail.com>
2016-07-13 22:38:15 -0700

    Add .zshrc

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ A collection of dotfiles to customize bash shell.
 Installation
 ------------
 
-- Clone repo with `git clone https://github.com/itwalton/dotfiles`
-
+- Clone rep to directory of your choice
 - Nav to installation directory and run `./install.sh`
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ function exec() {
   ln -s $PWD/aliases $HOME/.aliases
   ln -s $PWD/gitconfig $HOME/.gitconfig
   ln -s $PWD/bash_profile $HOME/.bash_profile
+  ln -s $PWD/zshrc $HOME/.zshrc
 }
 
 if [ -e $HOME/.aliases -o -e $HOME/.gitconfig -o -e $HOME/.bash_profile ]; then

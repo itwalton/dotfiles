6985f6b5d0970ca4a8d28eb603ae3f1bfda028ce
Ian Walton <itwalton90@gmail.com>
2017-01-25 16:01:34 -0600

    Add brew dependencies; Move symbolic links to dotfiles to load-files.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,1 @@
-# Use provided aliases unless one exists
-function exec() {
-  ln -s $PWD/files/aliases $HOME/.aliases
-  ln -s $PWD/files/gitconfig $HOME/.gitconfig
-  ln -s $PWD/files/bash_profile $HOME/.bash_profile
-  ln -s $PWD/files/gitexcludes $HOME/.gitexcludes
-  ln -s $PWD/files/vimrc $HOME/.vimrc
-}
-
-if [ -e $HOME/.aliases -o -e $HOME/.gitconfig -o -e $HOME/.bash_profile ]; then
-  echo "Files already exist. Backing up."
-  exit 1
-fi
-
-exec;
-
-echo "Loaded."
-unset exec;
+./load-dependencies.sh && ./load-files.sh
---
+++ load-dependencies.sh
@@ -0,0 +1,24 @@
+# Install Homebrew
+if [ ! $(which brew) ]; then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
+
+brew update
+
+# Recipes
+RECIPES=(bash bash-git-prompt bash-completion vim wget git node gpg)
+
+brew install ${RECIPES[@]}
+brew cleanup
+
+# Casks
+if [ ! brew info brew-cask &>/dev/null ]; then
+  brew tap caskroom/cask
+fi
+
+CASKS=(google-chrome sublime-text evernote spotify divvy flycut)
+brew cask install ${CASKS[@]}
+
+# Global npm dependencies
+npm install -g dwupload

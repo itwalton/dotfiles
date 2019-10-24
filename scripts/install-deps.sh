c07d557ba52eba84f0f9c0554c938b9e1c4fd54e
Ian Walton <itwalton90@gmail.com>
2019-10-23 21:31:18 -0500

    add zsh autocompletion and syntax highlighting

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -18,6 +18,9 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
 # iterm
 if [ ! -d $HOME/.iterm2_shell_integration ]; then
   echo "Installing oh-my-zsh..."
